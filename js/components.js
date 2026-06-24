7db711e099d4b0c06149e523f320c3a82aed99cd
Neuman  Jen <jensheng65@gmail.com>
2026-06-24 16:21:23 +0800

    Update components.js

--- js/components.js
+++ js/components.js
@@ -133,10 +133,6 @@ function buildChrome() {
           <h5>Contact</h5>
           <p>龍馬生技 Ryoma Biotechnology</p>
           <p>新北市土城區自強街 8 號</p>
-         /*
-          <a href="tel:+886227800227">+886 2 2780 0227</a>
-          <a href="mailto:hello@ryoma.bio">hello@ryoma.bio</a>
-         */
         </div>
       </div>
       <div class="footer__bottom">
@@ -144,7 +140,11 @@ function buildChrome() {
         <p class="disclaimer">AcroViz is FDA-cleared & TFDA-certified. Information presented is for educational purposes and is not a substitute for professional medical advice, diagnosis, or treatment.</p>
       </div>
     </div>`;
+       /* <a href="tel:+886227800227">+886 2 2780 0227</a> */
+       /* <a href="mailto:hello@ryoma.bio">hello@ryoma.bio</a> */
   document.body.appendChild(footer);
+
+   
 
   /* interactions */
   const burger = nav.querySelector(".nav__burger");
